e5d60aa162ad0d1c2f99622d7fea210f4aedef13
Sierka02 <n2kasi00@students.oamk.fi>
2024-12-01 12:28:30 +0200

    Endpoints added

--- server.js
+++ server.js
@@ -5,3 +5,23 @@ var app = express()
 app.listen(3001, () => {
     console.log('server is running...');
 })
+
+app.get('/API', (req,res) => {
+
+})
+
+app.get('/user', (req,res) => {
+    
+})
+
+app.get('/genre', (req,res) => {
+    
+})
+
+app.get('/movie', (req,res) => {
+    
+})
+
+app.get('/review', (req,res) => {
+    
+})
