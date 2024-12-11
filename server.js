61517ddae85fce8fff5512a06fe3f67f3e12972a
Sierka02 <n2kasi00@students.oamk.fi>
2024-12-11 12:50:24 +0200

    Made the get parameters and created pg_connection file

--- server.js
+++ server.js
@@ -1,44 +1,51 @@
 import express from 'express'
-import pg from 'pg'
-import dotenv from 'dotenv'
-
-dotenv.config()
+import { pgPool } from './pg_connection.js';
 
 const app = express()
-
-const {Client} = pg
 
 app.listen(3001, () => {
     console.log('server is running...');
 })
 
 //get all movies
-app.get('/', (req,res) => {
-
-})
-
-// add new movie
-app.get('/', (req,res) => {
-
+app.get('/movies', async (req,res) => {
+    
+    try {
+        const result = await pgPool.query('SELECT * FROM movie')
+        res.json(result.rows)
+    } catch (error) {
+       res.status(400).json ({error: error.message})
+    }
 })
 
 // get movie by id
-app.get('/', (req,res) => {
+app.get('/movies/:id', async (req,res) => {
+    const { id } = req.params
+
+    try {
+        const result = await pgPool.query('SELECT * FROM movie WHERE id = $1', [id])
+        res.json(result.rows)
+    } catch (error) {
+        res.status(400).json ({error: error.message})
+    }
 
 })
 
-
-const client = new Client()
-
-connect()
-
-async function connect() {
+// add new movies
+app.post('/movies', async (req,res) => {
+    
+    const id = req.body.id
+    const name = req.body.name
+    const year = req.body.year
+    const genre = req.body.genre
+    
     try {
-        await client.connect()
-        console.log('Database connected...');
-        
-
+        await pgPool.query(
+            'INSERT INTO movie VALUES ($1,$2,$3,$4)', [id, name, year, genre])
+        res.end();
     } catch (error) {
-        console.log(error.message);   
+       res.status(400).json({error: error.message})
     }
-}
+
+
+})
